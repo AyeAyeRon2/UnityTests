4f6b7b158c3bdff14bc0e4b08973447464b6f2d4
Aaron <1020115@student.sae.edu.au>
2020-10-14 10:51:02 +1100

    assets

---
+++ Tests/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+
+        Debug.Log("Play Button Pressed");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Game exited");
+    }
+}
---
+++ Tests/Assets/Scripts/PauseState.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseState : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+
+    public bool pauseMenuActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseUI.SetActive(false);
+        pauseMenuActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenuActive)
+            {
+                Resume();
+            }
+
+            else
+            {
+                GamePaused();
+
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        pauseMenuActive = false;
+    }
+    public void GamePaused()
+    {
+        pauseUI.SetActive(true);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        pauseMenuActive = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
+    }
+}
